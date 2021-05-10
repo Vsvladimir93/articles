8c2fbb5b70200c698216adc0e1a20c07cd4b3c16
Vladimir Vranceanu <vsvladimir93@gmail.com>
2021-05-10 12:59:38 +0300

    Replace Optional isPresent() to map() and orElseGet()

--- src/main/java/com/petprojects/articles/controller/TopicController.java
+++ src/main/java/com/petprojects/articles/controller/TopicController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("topic")
@@ -24,39 +23,33 @@ public class TopicController {
     public ResponseEntity<Topic> create(@Valid @RequestBody Topic topic) {
         // TODO Validate body, ignore id
         // TODO Return created topic
-        return new ResponseEntity(service.create(topic), HttpStatus.CREATED);
+        return new ResponseEntity<>(service.create(topic), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Topic>> findAll() {
-        return new ResponseEntity<List<Topic>>(service.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Topic> findById(@PathVariable Long id) {
-        Optional<Topic> result = service.findById(id);
-        if (result.isPresent()) {
-            return new ResponseEntity(result.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
-        }
+        return service.findById(id)
+                .map(topic -> new ResponseEntity<>(topic, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody Topic topic) {
+    public ResponseEntity<Topic> update(@PathVariable Long id, @RequestBody Topic topic) {
         // TODO Validate body with id
         // TODO Return updated topic
-        Optional<Topic> result = service.update(id, topic);
-        if (result.isPresent()) {
-            return new ResponseEntity(result.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
-        }
+        return service.update(id, topic)
+                .map(updatedTopic -> new ResponseEntity<>(updatedTopic, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        return new ResponseEntity<Void>(service.delete(id) ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(service.delete(id) ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
     }
 
 }
